893f2b602eb3bf9238958afaae350ef2575b185f
heyho1337 <heyho1337@gmail.com>
2025-08-07 13:43:57 +0200

    logs

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+error_log("INDEX: ");
+error_log($_SERVER['HTTP_AUTHORIZATION']);
+error_log(print_r($_SERVER));
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
